8f32d87e9420bf6ac2aa56064fcff822a9b977b8
VaIice <rlatmdgus499@gmail.com>
2024-09-20 14:51:53 +0900

    [✨feat]: 새글 작성 api 추가

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const app = express()
 app.use(express.static(__dirname + '/public'))
 
 // MongoDB 연결
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
+// env 도와주는 라이브러리
+require('dotenv').config();
 let db;
 // deployment
 const url = process.env.DB_URL;
@@ -24,11 +26,17 @@ new MongoClient(url).connect().then((client)=>{
     console.log(err)
   })
 
+// MongoDB Id 사용
+const { ObjectId } = require('mongodb') 
+
 // ejs - html에 데이터 삽입 가능
 app.set('view engine', 'ejs');
 
+// 클라이언트가 데이터 보내면 .body로 꺼내기 가능
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) 
+
 //////////////////////// 서버 ////////////////////////
-// 메인 페이지 접속 시 실행
 app.get('/', (req, res) => {
     res.send('안녕')
 })
@@ -46,7 +54,39 @@ app.get('/list', async (req, res) => {
     res.render('list.ejs', {list: result})
 })
 
-app.get('/time', async (req, res) => {
-    let time = new Date();
-    res.render('time.ejs', {time})
+app.get('/write', (req, res) => {
+    res.render('write.ejs');
+})
+
+app.post('/write/add', async (req, res) => {
+    try {
+        const body = req.body;
+        const data = {
+            title: body.title,
+            content: body.content
+        }
+        if (!data.title || !data.content) {
+            res.send('내용을 입력해주세요.');
+        }
+        // collection에 document 기록
+        if (res.statusCode === 200) {
+            let result = await db.collection('write').insertOne(data)
+            res.redirect('/list')
+        }
+        else {
+            res.redirect('/')
+        }
+    }
+    catch {
+        res.redirect('/')
+    }
+})
+
+app.get('/detail/:id', async (req, res) => {
+    let result = await db.collection('write').findOne({ _id: new ObjectId('') }) 
+    const data = {
+        title: result.title,
+        content: result.content
+    }
+    res.render('detail.ejs', data);
 })
